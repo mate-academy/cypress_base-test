cbdc652534c346c581a655bba63da7708eeb3dc0
LinaDagaieva <Lina.yashchenko96@gmail.com>
2021-08-12 20:06:20 +0300

    Cypress lesson 4

---
+++ cypress/integration/examples/cypress_actions.js
@@ -0,0 +1,34 @@
+const user = {
+    username: 'test',
+    password: 'Qwerty123!'
+}
+
+describe('', () => {
+    beforeEach(() => {
+        cy.visit('https://demoqa.com/login');
+        cy.get('#userName').type(user.username);
+        cy.get('#password').type(user.password);
+        cy.get('#login').click();
+    });
+
+it('', () =>{
+    cy.url().should('include', '/profile');
+    cy.get('#userName-value').should('contain', 'test');
+    cy.contains('#item-2', 'Book Store').click();
+    cy.get('#searchBox').type('Speaking JavaScript');
+    cy.get('[id="see-book-Speaking JavaScript"]').click();
+    cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Book added to your collection.`)
+       });
+});
+it('', () =>{
+    cy.contains('#item-3', 'Profile').click();
+    cy.contains('.rt-tr-group', 'Speaking JavaScript')
+        .find('#delete-record-undefined').click();
+    cy.get('#closeSmallModal-ok').click();
+    cy.on('window:alert 2', (str) => {
+        expect(str).to.equal('Book deleted.');
+        });
+    });
+});
