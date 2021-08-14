85e715b02114d77ac7ccfe574806ae4c2fd49939
LinaDagaieva <Lina.yashchenko96@gmail.com>
2021-08-14 16:11:10 +0300

    Cypress-4 Advanced

--- cypress/integration/examples/cypress_actions.js
+++ cypress/integration/examples/cypress_actions.js
@@ -1,28 +1,37 @@
-const user = {
-    username: 'test',
-    password: 'Qwerty123!'
-}
+/// <reference types="cypress" />
 
 describe('', () => {
-    beforeEach(() => {
-        cy.visit('https://demoqa.com/login');
-        cy.get('#userName').type(user.username);
-        cy.get('#password').type(user.password);
-        cy.get('#login').click();
-    });
+    const user = {
+        username: 'test',
+        password: 'Qwerty123!'
+    };
 
-it('', () =>{
+    const book = {
+        name: 'Speaking JavaScript',
+        author: 'Axel Rauschmayer',
+        publisher: 'O\'Reilly Media'
+     };
+
+
+it('successsfully login', () => {
+    cy.visit('/profile');
     cy.url().should('include', '/profile');
-    cy.get('#userName-value').should('contain', 'test');
+    cy.get('#userName-value').should('contain', user.username);
+});
+
+it('add book to profile', () =>{
     cy.contains('#item-2', 'Book Store').click();
-    cy.get('#searchBox').type('Speaking JavaScript');
-    cy.get('[id="see-book-Speaking JavaScript"]').click();
+    cy.get('#searchBox').type(book.name);
+    cy.contains('.rt-tr.-odd', book.name)
+    .should('contain', book.author).and('contain', book.publisher);
+    cy.get('.action-buttons').click();
     cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
     cy.on('window:alert', (str) => {
         expect(str).to.equal(`Book added to your collection.`)
        });
-});
-it('', () =>{
+    });
+
+it('delete book from profile', () =>{
     cy.contains('#item-3', 'Profile').click();
     cy.contains('.rt-tr-group', 'Speaking JavaScript')
         .find('#delete-record-undefined').click();
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,10 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+import './commands/login'
+import './beforeAll'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.Cookies.defaults({
+    preserve: ['token', 'expires', 'userName', 'userID']
+});
